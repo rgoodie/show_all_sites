0372e031233c29fbcd19807831ef1ea0d581e8a1
rag227 <rgoodie@noreply>
2017-03-01 10:10:11 -0600

    Added report. Dropped blocks.

--- MultiSites.class.php
+++ MultiSites.class.php
@@ -16,15 +16,20 @@ class MultiSites
 
     public function __construct()
     {
-        $this->master_url_pattern = explode("\n", variable_get('show_all_sites_URLpattern', ''));
+        $this->master_url_pattern = explode("\n", trim(variable_get('show_all_sites_URLpattern', '')));
         $this->raw_folders = $this->getRawFolders();
-        $this->makeLinksIfPossible();
+        $this->refined_links = $this->makeLinksIfPossible();
+
+
 
 
 
     }
 
-
+    /**
+     * @return array of folders in /sites/
+     * @throws Exception
+     */
     private function getRawFolders()
     {
 
@@ -45,33 +50,35 @@ class MultiSites
             return $item;
         }, $raw_folders);
 
-        // Try to convert to URL based on above master url patterns
-        for ($i = 0; $i < count($raw_folders); $i++) {
-
-            // step through each URL pattern
-            foreach ($this->master_url_pattern as $pattern) {
-                $pattern_with_trailing_dot = $pattern . ".";
-                $url_as_link = sprintf("https://%s/", $pattern);
-                $raw_folders[$i] = str_replace($pattern_with_trailing_dot, $url_as_link, $raw_folders[$i]);
-            }
-        }
-
         sort($raw_folders);
-
 
         return $raw_folders;
     }
 
-
+    /*
+     * Refines links. Creates new array. If there is a match with the module settings url master pattern, then
+     * this attempts to create a link.
+     */
     private function makeLinksIfPossible()
     {
 
-        for ($i = 0; $i < count($this->raw_folders); $i++) {
-            if (stristr($this->raw_folders[$i], 'http')) {
-                $this->refined_links[] = l($this->raw_folders[$i], $this->raw_folders[$i]);
-            }
+        // if url pattern not set
+        if ((count($this->master_url_pattern) < 1 ) || (strlen($this->master_url_pattern[0]) < 3 ))  {
+            return array(t('URL pattern not set in module settings.'));
         }
 
+
+
+        $refind_links = array();
+        for ($i = 0; $i < count($this->raw_folders); $i++) {
+            foreach($this->master_url_pattern as $pattern) {
+                if (stristr($this->raw_folders[$i], $pattern)) {
+                    $new_url = str_replace($pattern . '.', 'https://' . $pattern . '/', $this->raw_folders[$i] ) ;
+                    $refind_links[] = l($new_url, $new_url);
+                }
+            }
+        }
+        return $refind_links;
     }
 
 
